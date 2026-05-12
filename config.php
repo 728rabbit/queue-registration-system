e800394a0af1cd5ffb2d249ec753a9f8b2639229
Kaiyun <kaiyunchan2012@hotmail.com>
2026-05-12 15:10:39 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'redis' => [
+        'host' => 'redis-123456.c2345678.ap-southeast-1.cloud.redislabs.com',
+        'port' => 12345,
+        'password' => '你的Redis密碼'
+    ],
+    'mysql' => [
+        'host' => 'localhost',
+        'user' => 'your_username',
+        'password' => 'your_password',
+        'database' => 'queue_system'
+    ]
+];
+?>
